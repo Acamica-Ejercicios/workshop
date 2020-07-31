77d5df864351b34c651aab34418093e0f075a557
Choco <choco@Choco.local>
2020-07-30 21:35:22 -0300

    choco

--- index.js
+++ index.js
@@ -7,30 +7,32 @@ const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 
-function verificarUsuarioLoggeado(req, res, next){
+function verificarUsuarioLoggeado(req, res, next) {
     let idUsuario = req.header['Authorization']
 }
 
 //ENDPOINTS
-app.post('/verificacion', (req, res)=>{
+app.post('/verificacion', (req, res) => {
     let nombre = req.body.nombre
     let clave = req.body.clave
     let resultado = usuarios.verificar(nombre, clave)
-    if(resultado != null){
+    if (resultado != null) {
         console.log('verificado')
         res.status(200).json(resultado)
-    } else{ 
+    } else {
         res.status(404).send('usuario no encontrado')
     }
 })
 
-app.get('/usuarios', (req, res)=>{
+app.get('/usuarios', (req, res) => {
     req.header['Authorization']
     res.status(200).json(usuarios.usuarios)
 })
 
 
 //INITIALAZE SERVER
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Servidor iniciado')
 })
+
+//choco
